41dfafbd0396fb3de27587ee037ee591afcd143b
cjiph <cjiph@55a4958a-cc0d-0410-919a-9f8291766008>
2006-02-27 22:29:34 +0000

    Moved to SVN due to CVS crapness.
    git-svn-id: https://afuse.svn.sourceforge.net/svnroot/afuse@3 55a4958a-cc0d-0410-919a-9f8291766008

---
+++ remove-cruft.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+make distclean
+rm -rf -- -/ autom4te.cache Makefile Makefile.in aclocal.m4 config.h.in config.h afuse*.tar.gz stamp-h* mkinstalldirs config.log config.status configure depcomp  install-sh  missing src/Makefile.in compat/Makefile.in *.bak *~ .in config.guess  config.sub
